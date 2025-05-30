44886e899fe58362acb59938d85f0faeed576025
aditya123473892 <adityathakur6199@gmail.com>
2025-05-30 13:20:00 +0530

    add

--- Src/routes/UserRoutes.js
+++ Src/routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
 const adminAuth = require("../middlewares/adminmiddleware");
-const UserController = require("../controller/usercontroller");
+const UserController = require("../controller/UserController");
 
 router.get("/allusers", UserController.getAllusers);
 
